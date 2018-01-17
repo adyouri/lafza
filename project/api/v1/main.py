fa0f3c89c154b72f70f1aa5575a46da51d5f0f36
Abdelhadi Dyouri <raryat@gmail.com>
2018-01-17 19:30:49 +0000

    Add post new translation

--- project/api/v1/main.py
+++ project/api/v1/main.py
@@ -1,11 +1,16 @@
 from flask import Blueprint
-from flask_restplus import Api, Resource
+from flask_restplus import Api, Resource, fields
 from sqlalchemy.exc import IntegrityError
 from project.models import Term, Translation, db
 import project.core as core
 
 main_api = Blueprint('main_api', __name__)
 api = Api(main_api)
+
+translation_model = api.model('Translation', {
+                              'translation': fields.String,
+                              'term_id': fields.Integer,
+                              })
 
 
 @api.route('/<string:term>')
@@ -31,9 +36,14 @@ class TranslationsAPI(Resource):
         all_translations = Translation.query.all()
         return core.translations_repr(all_translations)
 
-    def post(self, translation, term_id):
-        translation = Translation(translation=translation, term_id=term_id)
-        db.session.add(translation)
+    @api.expect(translation_model)
+    def post(self):
+        received_translation = api.payload['translation']
+        received_term_id = api.payload['term_id']
+        new_translation = Translation(translation=received_translation,
+                                      term_id=received_term_id)
+        db.session.add(new_translation)
         db.session.commit()
-        term = Term.query.filter(translation.term_id).first()
+        term = Term.query.get(new_translation.term_id)
+        term.translations.append(new_translation)
         return term.dictionary(), 201
