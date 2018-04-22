540bfc937a302810b35603eee23c6d3ef6cc3d10
Abdelhadi Dyouri <raryat@gmail.com>
2018-04-22 22:08:21 +0100

    Clarify schemas.py comments

--- project/schemas.py
+++ project/schemas.py
@@ -9,32 +9,48 @@ ma = Marshmallow()
 
 
 def tags_to_tag_ids(tags):
-    '''A generator that takes a list of string tag names
+    '''
+    A generator that takes a list of string tag names
     and yields IDs of these tags if they exist
-    or creates new tags and returns their IDs'''
+    or creates new tags and returns their IDs
+    :param: tags: a list of string tag names
+    '''
     for tag_name in tags:
+        # Query the tag
         tag = Tag.query.filter_by(name=tag_name).one_or_none()
         if not tag:
+            # If the tag does not exist, create it and yield the new tag's ID
             new_tag = Tag(name=tag_name)
             db.session.add(new_tag)
             db.session.commit()
             yield new_tag.id
         else:
+            # If the tag exists, return it's ID
             yield tag.id
 
 
 class TranslationSchema(ma.ModelSchema):
+    ''' Translation Marshmallow schema '''
+    # Make term_id required
     term_id = field_for(Translation, 'term_id',
                         required=True,
                         )
 
     class Meta:
+        '''
+            The Flask-Marshmallow Meta class for generating the fields
+            directly from SQLAlchemy models
+        '''
         model = Translation
         sqla_session = db.session
 
     def add_translation_tags(self, data):
+        ''' A function that adds the tag IDs to the Marshmallow data object '''
+        # Save the current tags
         tag_names = data['tags']
+        # tags_to_tag_ids() converts the tag names list to a list of tag IDs
         data['tags'] = list(tags_to_tag_ids(tag_names))
+        # Return the modified data
         return data
 
     @pre_load
@@ -55,7 +71,7 @@ class TermSchema(ma.ModelSchema):
     full_term = field_for(Term, 'full_term',
                           validate=validate.Length(min=2))
 
-    # Flask-Marshmallow SQLAlchemy Integration
+    # Flask-Marshmallow SQLAlchemy integration
     class Meta:
         model = Term
     translations = ma.Nested(TranslationSchema, many=True)
