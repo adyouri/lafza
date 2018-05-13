a4e70cb92b5c995345beb2d48e160712387e6bfe
Abdelhadi Dyouri <raryat@gmail.com>
2018-05-13 21:31:33 +0000

    Add is_blacklisted function

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,20 @@
 from flask import Flask
 from flask_migrate import Migrate
 
+# A set for storing blacklisted JWT tokens (This should use Redis instead)
+jwt_blacklist = set()
+
+
+def is_blacklisted(jti):
+    """
+    check whether a JWT token is blacklisted or not.
+
+    This is Flask-Praetorian specific (see guard.init_app() below).
+
+    :param jti: The token's JTI (which is unique for each token)
+    """
+    return jti in jwt_blacklist
+
 
 def create_app(config_file):
     app = Flask(__name__)
@@ -14,7 +28,7 @@ def create_app(config_file):
     # flask-praetorian
     from project.api.v1.users import guard
     from project.models import User
-    guard.init_app(app, User)
+    guard.init_app(app, User, is_blacklisted=is_blacklisted)
 
     # flask-marshmallow
     from project.schemas import ma
