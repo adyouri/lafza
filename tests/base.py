516c98e4cccb1a0babe67397372e32db73b5ac64
Abdelhadi Dyouri <raryat@gmail.com>
2018-05-18 03:04:08 +0000

    Remove useless function

--- tests/base.py
+++ tests/base.py
@@ -2,36 +2,6 @@ import json
 from flask import url_for
 
 
-def make_json_request(http_method, endpoint, headers={}, data=None):
-    """
-    Return a response object from a Flask test client http method.
-
-    :param http_method: HTTP method to be used for the request (ie. client.get)
-    :param endpoint: Request Endpoint (ie. 'main_api.terms')
-    :param headers: HTTP headers
-    :param data: Data to be sent if it's a POST/PATCH request
-
-    :returns: Response object
-
-    Usage::
-
-        >>> test_client = app.test_client()
-        >>> with app.test_request_context(): # request context for url_for()
-        ...     res = make_json_request(http_method=test_client.get,
-        ...                             endpoint='main_api.terms')
-        ...
-        >>> res.status_code # UNAUTHORIZED, because we haven't authenticated
-        401
-    """
-    response = http_method(
-                           url_for(endpoint),
-                           content_type='application/json',
-                           data=data,
-                           headers=headers,
-                           )
-    return response
-
-
 def length_error(_min, _max):
     """
     Return a Marshmallow length error according to the min and max values.
@@ -50,8 +20,7 @@ def valid_jwt_token(client):
     :returns: A JWT token.
     """
     # User JSON data
-    # The user was added to the database in conftest.py
-
+    # This user was added to the database in conftest.py
     user_data = json.dumps(
             dict(
                 username='test',
--- todo.md
+++ todo.md
@@ -1,4 +1,3 @@
-# Change self.client.get()/post() to make_json_request()
 # Use Redis for blacklisting
 # Follow https://www.python.org/dev/peps/pep-0257/
 # Comment and DRY tests
