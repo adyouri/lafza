1942ec8a81fd7e2a2f5e974a0b644966ee469f4c
Abdelhadi Dyouri <raryat@gmail.com>
2018-05-20 03:09:58 +0000

    Check if JTI is in the Redis blacklist using the `in` operator

--- project/auth.py
+++ project/auth.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 import redis
 from flask import current_app
 from flask_praetorian import Praetorian
+
 guard = Praetorian()
 
 # A set for storing blacklisted JWT tokens (This should use Redis instead)
@@ -9,7 +10,19 @@ guard = Praetorian()
 
 
 class BlackList:
-    """ This class is supposed to behave like a Python set """
+    """
+    This class is supposed to behave like a Python set.
+
+    So that Redis can be easily replaced with a regular Python set.
+
+    Usage::
+        >>> jwt_blacklist = BlackList()
+        >>> jwt_blacklist.add('value')
+        >>> 'value' in jwt_blacklist
+        True
+        >>> 'another_value' in jwt_blacklist
+        False
+    """
     _redis_blacklist = redis.StrictRedis(host='localhost',
                                          port=6379,
                                          db=0,
@@ -18,7 +31,7 @@ class BlackList:
 
     def add(self, jti):
         """
-        Add a jti to the redis blacklist
+        Add a jti to the Redis blacklist
 
         Will also set it to automatically expire after the token expires
         :param jti: The token's JTI (which is unique for each token)
@@ -29,26 +42,30 @@ class BlackList:
                                   expire_time,
                                   )
 
-    def get(self, key):
-        """ Get a key's value from the redis server """
-        return self._redis_blacklist.get(key)
+    # def get(self, key):
+    #     """ Get a key's value from the redis server """
+    #     return self._redis_blacklist.get(key)
+
+    def __contains__(self, key):
+        result = self._redis_blacklist.get(key)
+        return bool(result)  # True if the key exists, False if result is None
 
 
 jwt_blacklist = BlackList()
 
 
-# def is_blacklisted(jti):
-#     """
-#     check whether a JWT token is blacklisted or not.
-#
-#     This is Flask-Praetorian specific (see guard.init_app() below).
-#
-#     :param jti: The token's JTI (which is unique for each token)
-#     """
-#     return jti in jwt_blacklist
-
-
 def is_blacklisted(jti):
-    entry = jwt_blacklist.get(jti)
-    # A blacklisted jti has the value 'blacklisted'
-    return entry == 'blacklisted'
+    """
+    check whether a JWT token is blacklisted or not.
+
+    This is Flask-Praetorian specific (see guard.init_app() below).
+
+    :param jti: The token's JTI (which is unique for each token)
+    """
+    return jti in jwt_blacklist
+
+
+# def is_blacklisted(jti):
+#     entry = jwt_blacklist.get(jti)
+#     # A blacklisted jti has the value 'blacklisted'
+#     return entry == 'blacklisted'
--- todo.md
+++ todo.md
@@ -1,5 +1,4 @@
-# Use Redis for blacklisting
-# Use __in__ magic method to make the redis server behave like a python list
+# Test BlackList
 # Test that black listed token expire in redis after 3 seconds (refresh lifespan)
 # Follow https://www.python.org/dev/peps/pep-0257/
 # Comment and DRY tests
